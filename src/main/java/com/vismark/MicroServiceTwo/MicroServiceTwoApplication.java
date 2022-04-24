1beab486cdb474180e8fb02c5a7299db66d14ed8
Vismark Juarez <vismarkjuarez@gmail.com>
2022-04-24 12:56:46 -0500

    Initializing project.

---
+++ src/main/java/com/vismark/MicroServiceTwo/MicroServiceTwoApplication.java
@@ -0,0 +1,13 @@
+package com.vismark.MicroServiceTwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServiceTwoApplication.class, args);
+	}
+
+}
